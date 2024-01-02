49bc810233cc186e041dcfec29e0da7a35df2065
Gabriel Mendes <135557387+Gabriels220@users.noreply.github.com>
2024-01-02 19:22:43 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,58 @@
+let amigos = [];
+
+function adicionar(){
+    let amigo = document.getElementById('nome-amigo');
+    if (amigo.value == '') {
+        alert('Informe o nome do amigo');
+        return;
+    }
+
+    if (amigos.includes(amigo.value)) {
+        alert('Nome já adicionado!');
+        amigo.value = '';
+        return;
+    }
+
+    let listaAmigos = document.getElementById('lista-amigos');
+    amigos.push(amigo.value);
+
+    if (listaAmigos.textContent == '') {
+        listaAmigos.textContent = amigo.value;
+    } else{
+        listaAmigos.textContent = listaAmigos.textContent + ', ' + amigo.value;
+    }
+    amigo.value = '';
+}
+
+function sortear(){
+    if (amigos.length <= 3) {
+    alert('Adicione pelo menos 4 amigos');
+    return;
+    }
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+        sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[0] + '<br/>';
+    }   else {
+        sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[i + 1] + '<br/>';
+    }
+    }
+}
+
+function embaralha(listaAmigos){
+    for (let indice = listaAmigos.length; indice; indice--){
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [listaAmigos[indice - 1], listaAmigos[indiceAleatorio]] =
+        [listaAmigos[indiceAleatorio], listaAmigos[indice -1]];
+    }
+}
+
+function reiniciar(){
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
